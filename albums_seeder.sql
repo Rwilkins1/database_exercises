ef0eaec031959540e435c9a25b8ba95dd442d03b
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-20 10:06:49 -0600

    completed with exercise with a single insert

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,67 +1,35 @@
 USE codeup_test_db;
 INSERT INTO albums (artist, name, sales)
-VALUES ('Michael Jackson', 'Thriller', 43.3);
-INSERT INTO albums (artist, name, sales)
-VALUES ('AC/DC', 'Back in Black', 25.9);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Pink Floyd', 'The Dark Side of the Moon', 22.7);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Whitney Houston', 'The Bodyguard', 27.4);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Meat Loaf', 'Bat Out of Hell', 20.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Eagles', 'Their Greatest Hits (1971-1975', 32.2);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Bee Gees', 'Saturday Night Fever', 19);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Fleetwood Mac', 'Rumors', 27.9);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Shania Twain', 'Come on Over', 29.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Led Zeppelin', 'Led Zeppelin IV', 29.0);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Spice Girls', 'Spice', 28);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Alanis Morissette', 'Jagged Little Pill', 24.8);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Celine Dion', 'Falling into You', 20.2);
-INSERT INTO albums (artist, name, sales)
-VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 13.1);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Eagles', 'Hotel California', 21.5);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Mariah Carey', 'Music Box', 19.5);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Various Artists', 'Dirty Dancing', 17.9);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Celine Dion', 'Let''s Talk About Love', 19.3);
-INSERT INTO albums (artist, name, sales)
-VALUES ('The Beatles', '1', 21.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Adele', '21', 21.3);
-INSERT INTO albums (artist, name, sales)
-VALUES ('The Beatles', 'Abbey Road', 14.4);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 19.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Dire Straights', 'Brothers in Arms', 18.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Whitney Houston', 'Whitney Houston', 17.2);
-INSERT INTO albums (artist, name, sales)
-VALUES ('James Horner', 'Titanic: Music from the Motion Picture', 18.1);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Madonna', 'The Immaculate Collection', 19.4);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Metallica', 'Metallica', 19.9);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Michael Jackson', 'Bad', 18.4);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Michael Jackson', 'Dangerous', 16.3);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Nirvana', 'Nevermind', 16.7);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Pink Floyd', 'The Wall', 17.6);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Santana', 'Supernatural', 20.5);
-INSERT INTO albums (artist, name, sales)
-VALUES ('Guns N'' Roses', 'Appetite for Destruction', 21.3);
+VALUES ('Michael Jackson', 'Thriller', 43.3),
+       ('AC/DC', 'Back in Black', 25.9),
+       ('Pink Floyd', 'The Dark Side of the Moon', 22.7),
+       ('Whitney Houston', 'The Bodyguard', 27.4),
+       ('Meat Loaf', 'Bat Out of Hell', 20.6),
+       ('Eagles', 'Their Greatest Hits (1971-1975', 32.2),
+       ('Bee Gees', 'Saturday Night Fever', 19),
+       ('Fleetwood Mac', 'Rumors', 27.9),
+       ('Shania Twain', 'Come on Over', 29.6),
+       ('Led Zeppelin', 'Led Zeppelin IV', 29.0),
+       ('Spice Girls', 'Spice', 28),
+       ('Alanis Morissette', 'Jagged Little Pill', 24.8),
+       ('Celine Dion', 'Falling into You', 20.2),
+       ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 13.1),
+       ('Eagles', 'Hotel California', 21.5),
+       ('Mariah Carey', 'Music Box', 19.5),
+       ('Various Artists', 'Dirty Dancing', 17.9),
+       ('Celine Dion', 'Let''s Talk About Love', 19.3),
+       ('The Beatles', '1', 21.6),
+       ('Adele', '21', 21.3),
+       ('The Beatles', 'Abbey Road', 14.4),
+       ('Bruce Springsteen', 'Born in the U.S.A.', 19.6),
+       ('Dire Straights', 'Brothers in Arms', 18.6),
+       ('Whitney Houston', 'Whitney Houston', 17.2),
+       ('James Horner', 'Titanic: Music from the Motion Picture', 18.1),
+       ('Madonna', 'The Immaculate Collection', 19.4),
+       ('Metallica', 'Metallica', 19.9),
+       ('Michael Jackson', 'Bad', 18.4),
+       ('Michael Jackson', 'Dangerous', 16.3),
+       ('Nirvana', 'Nevermind', 16.7),
+       ('Pink Floyd', 'The Wall', 17.6),
+       ('Santana', 'Supernatural', 20.5),
+       ('Guns N'' Roses', 'Appetite for Destruction', 21.3);
