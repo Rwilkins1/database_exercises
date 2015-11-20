0d0c28f41bb0c4bbcd71127438c042128e971aa3
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-20 15:07:12 -0600

    completed the second step of the update exercise

--- update_exercise.sql
+++ update_exercise.sql
@@ -1,4 +1,15 @@
 USE codeup_test_db;
-SELECT name AS "Album Names" FROM albums;
-SELECT name AS "Albums released before 1980" FROM albums WHERE release_date < 1980;
-SELECT name AS "Albums by Michael Jackson" FROM albums WHERE artist = "Michael Jackson";
+SELECT name AS "Album Names", sales FROM albums;
+UPDATE albums
+SET sales = (sales / 10);
+
+SELECT name AS "Albums released before 1980", release_date FROM albums WHERE release_date < 1980;
+UPDATE albums
+SET release_date = (release_date + 200)
+WHERE release_date < 1980;
+
+SELECT name AS "Albums by Michael Jackson", artist FROM albums WHERE artist = "Michael Jackson";
+UPDATE albums 
+SET artist = "Peter Jackson"
+WHERE artist = "Michael Jackson";
+SELECT name AS "Albums by Peter Jackson", artist FROM albums WHERE artist = "Peter Jackson";
