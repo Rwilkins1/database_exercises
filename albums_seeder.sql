a922d27d1ab52d8f7b776cda1b4883aaf0fd6f3f
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-20 10:04:22 -0600

    completed the exercise with several inserts

---
+++ albums_seeder.sql
@@ -0,0 +1,67 @@
+USE codeup_test_db;
+INSERT INTO albums (artist, name, sales)
+VALUES ('Michael Jackson', 'Thriller', 43.3);
+INSERT INTO albums (artist, name, sales)
+VALUES ('AC/DC', 'Back in Black', 25.9);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Pink Floyd', 'The Dark Side of the Moon', 22.7);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Whitney Houston', 'The Bodyguard', 27.4);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Meat Loaf', 'Bat Out of Hell', 20.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Eagles', 'Their Greatest Hits (1971-1975', 32.2);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Bee Gees', 'Saturday Night Fever', 19);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Fleetwood Mac', 'Rumors', 27.9);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Shania Twain', 'Come on Over', 29.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Led Zeppelin', 'Led Zeppelin IV', 29.0);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Spice Girls', 'Spice', 28);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Alanis Morissette', 'Jagged Little Pill', 24.8);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Celine Dion', 'Falling into You', 20.2);
+INSERT INTO albums (artist, name, sales)
+VALUES ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', 13.1);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Eagles', 'Hotel California', 21.5);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Mariah Carey', 'Music Box', 19.5);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Various Artists', 'Dirty Dancing', 17.9);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Celine Dion', 'Let''s Talk About Love', 19.3);
+INSERT INTO albums (artist, name, sales)
+VALUES ('The Beatles', '1', 21.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Adele', '21', 21.3);
+INSERT INTO albums (artist, name, sales)
+VALUES ('The Beatles', 'Abbey Road', 14.4);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Bruce Springsteen', 'Born in the U.S.A.', 19.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Dire Straights', 'Brothers in Arms', 18.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Whitney Houston', 'Whitney Houston', 17.2);
+INSERT INTO albums (artist, name, sales)
+VALUES ('James Horner', 'Titanic: Music from the Motion Picture', 18.1);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Madonna', 'The Immaculate Collection', 19.4);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Metallica', 'Metallica', 19.9);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Michael Jackson', 'Bad', 18.4);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Michael Jackson', 'Dangerous', 16.3);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Nirvana', 'Nevermind', 16.7);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Pink Floyd', 'The Wall', 17.6);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Santana', 'Supernatural', 20.5);
+INSERT INTO albums (artist, name, sales)
+VALUES ('Guns N'' Roses', 'Appetite for Destruction', 21.3);
