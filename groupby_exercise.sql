adf5fbc9e3e0659de5f5e3b722e1c8c18bd20fc0
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 15:44:19 -0600

    fourth step

--- groupby_exercise.sql
+++ groupby_exercise.sql
@@ -6,3 +6,8 @@ SELECT * FROM employees
 WHERE last_name LIKE '%E'
 AND last_name LIKE 'E%'
 GROUP BY last_name ASC;
+
+SELECT * FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name ASC;
