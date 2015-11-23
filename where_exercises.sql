18f5f192c7eca923fc4572f79d5f77da53a19ba4
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 13:51:59 -0600

    completed the third step of the second where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -4,6 +4,11 @@ AND (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya');
 
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE last_name LIKE ('E%')
+OR last_name LIKE ('%E');
+
 SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE ('E%');
