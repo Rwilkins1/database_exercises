215e1e8cd24c0baf96a890e3c1808d878aa70a0d
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 14:43:13 -0600

    completed the final step

--- orderby_exercises.sql
+++ orderby_exercises.sql
@@ -9,10 +9,10 @@ SELECT * FROM employees
 WHERE last_name LIKE ('%E%')
 ORDER BY emp_no DESC;
 
-SELECT first_name, last_name, hire_date, birth_date
-FROM employees
+SELECT * FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
-AND birth_date LIKE '%-12-25';
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC;
 
 SELECT first_name, last_name
 FROM employees
