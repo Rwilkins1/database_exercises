0b67ff1e27e9d35489a9b7589b5dab445d17dd27
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 13:48:48 -0600

    completed the first step of the second where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,6 +1,8 @@
 SELECT first_name, last_name
 FROM employees
-WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya';
 
 SELECT first_name, last_name
 FROM employees
