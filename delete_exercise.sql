4f2500070bc7f84cc1908e772c7e25af442fdd37
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 09:42:10 -0600

    completed the final steps of the delete exercise

--- delete_exercise.sql
+++ delete_exercise.sql
@@ -1,4 +1,4 @@
 USE codeup_test_db;
-SELECT name AS "Albums released after 1991" FROM albums WHERE release_date > 1991;
-SELECT name AS "Disco Albums" FROM albums WHERE genre = "Disco";
-SELECT name AS "Albums by AC/DC" FROM albums WHERE artist = "AC/DC";
+DELETE FROM albums WHERE release_date > 1991;
+DELETE FROM albums WHERE genre = "Disco";
+DELETE FROM albums WHERE artist = "AC/DC";
