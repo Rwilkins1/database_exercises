94248675697be2bcc1e17ad711ed619d61593252
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-23 09:34:20 -0600

    added the truncate in the album seeder

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,11 +1,12 @@
 USE codeup_test_db;
+TRUNCATE albums;
 INSERT INTO albums (artist, name, release_date, sales, genre)
 VALUES ('Michael Jackson', 'Thriller', 1982, 43.3, 'Pop'),
        ('AC/DC', 'Back in Black', 1980, 25.9, 'Hard Rock'),
        ('Pink Floyd', 'The Dark Side of the Moon', 1973, 22.7, 'Progressive Rock'),
        ('Whitney Houston', 'The Bodyguard', 1992, 27.4, 'Soundtrack'),
        ('Meat Loaf', 'Bat Out of Hell', 1977, 20.6, 'Hard Rock'),
-       ('Eagles', 'Their Greatest Hits (1971-1975', 1976, 32.2, 'Rock'),
+       ('Eagles', 'Their Greatest Hits (1971-1975)', 1976, 32.2, 'Rock'),
        ('Bee Gees', 'Saturday Night Fever', 1977, 19, 'Disco'),
        ('Fleetwood Mac', 'Rumors', 1977, 27.9, 'Soft Rock'),
        ('Shania Twain', 'Come on Over', 1997, 29.6, 'Country'),
