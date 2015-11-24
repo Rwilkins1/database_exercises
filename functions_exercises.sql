b1d03f707f4da67fe30518a6553252033b6cdbd1
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-24 14:08:11 -0600

    final step re-done

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -17,8 +17,9 @@ WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25'
 ORDER BY birth_date ASC, hire_date DESC;
 
-SELECT first_name, COUNT(last_name), last_name
+SELECT CONCAT(first_name, ' ', last_name) AS fullname, count(*) AS same
 FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%'
-GROUP BY last_name;
+GROUP BY fullname
+ORDER BY same DESC;
