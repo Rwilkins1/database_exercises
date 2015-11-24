ba33d7aeb29be83bfd6d31a04c2b45b4ad61b356
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-24 11:53:17 -0600

    third step

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -11,9 +11,8 @@ WHERE last_name LIKE '%E'
 AND last_name LIKE 'E%';
 
 
-SELECT first_name, last_name
+SELECT first_name, last_name, datediff(NOW(), hire_date)
 FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25'
-ORDER BY birth_date ASC, hire_date DESC
-LIMIT 5 OFFSET 45;
+ORDER BY birth_date ASC, hire_date DESC;
