8c383cd9184574df572c3cf671efa9d57b655615
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-24 13:27:24 -0600

    Final step

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -16,3 +16,9 @@ FROM employees
 WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
 AND birth_date LIKE '%-12-25'
 ORDER BY birth_date ASC, hire_date DESC;
+
+SELECT first_name, COUNT(last_name), last_name
+FROM employees
+WHERE last_name LIKE '%q%'
+AND last_name NOT LIKE '%qu%'
+GROUP BY last_name;
