b29e79e86ea6bd986f5177a832906376fbf6ee4f
Reagan Wilkins <Reagan@Reagans-MacBook-Pro.local>
2015-11-24 11:47:28 -0600

    first step

---
+++ functions_exercises.sql
@@ -0,0 +1,14 @@
+SELECT COUNT(gender), gender 
+FROM employees
+WHERE first_name = 'Irena'
+OR first_name = 'Vidya'
+OR first_name = 'Maya'
+GROUP BY gender ASC;
+
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45;
